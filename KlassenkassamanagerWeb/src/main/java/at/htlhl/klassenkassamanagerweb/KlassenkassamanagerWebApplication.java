c69439c94e873c3c6650c991298278b6c462b60b
Benjamin Suljevic <bsuljevic1809@gmail.com>
2023-11-16 11:58:28 +0100

    KlassenkassamanagerWeb created

---
+++ KlassenkassamanagerWeb/src/main/java/at/htlhl/klassenkassamanagerweb/KlassenkassamanagerWebApplication.java
@@ -0,0 +1,13 @@
+package at.htlhl.klassenkassamanagerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlassenkassamanagerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlassenkassamanagerWebApplication.class, args);
+	}
+
+}
---
+++ KlassenkassamanagerWeb/src/test/java/at/htlhl/klassenkassamanagerweb/KlassenkassamanagerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlhl.klassenkassamanagerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlassenkassamanagerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
