a4b43e5e8d2a9408ebb46c00510233a63d2fd870
Adrian Zeitlberger <adrian.zeitlberger@it.htlhl.at>
2023-11-16 12:24:40 +0100

    Created basic Database (Student, Class)

---
+++ KlassenkassamanagerWeb/src/main/resources/schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Student (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    firstname VARCHAR,
+    lastname VARCHAR,
+    depositAmount FLOAT,
+    toPayAmount FLOAT,
+    classId INT,
+    FOREIGN KEY (classId) REFERENCES Class(id)
+);
+
+CREATE TABLE Class (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    department VARCHAR,
+    schoolLevel INT
+);
